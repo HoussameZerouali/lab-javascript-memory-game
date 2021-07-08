8aca45f2f339cdcb22369a4fe4f5bd6b33cb6e41
Houssame Zerouali <azenax328@gmail.com>
2021-07-08 16:16:33 +0200

    finished the game logic

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked =0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    
+    for (let i = 0; i < this.cards.length; i++) {
+      let x=Math.floor(Math.random(0,this.cards.length))
+      let y=Math.floor(Math.random(0,this.cards.length))
+      let temporary = this.cards[i]
+      this.cards[i] = this.cards[x];
+      this.cards[x] = temporary;
+      
+    }
+    
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked += 1;
+    if(card1 == card2){
+      this.pairsGuessed += 1;
+      return true
+    }else{
+      return false
+    }
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed == (this.cards.length/2)){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
 
